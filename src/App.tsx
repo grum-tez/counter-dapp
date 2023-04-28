5f5d3a2fc7390bc97ab0b8ef607a86a084a2837a
grum-tez <grum.tez@gmail.com>
2023-04-28 15:33:18 +1200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { SettingsProvider } from './contexts/Settings'
+import { TaquitoProvider } from './contexts/Taquito'
+import { BeaconProvider } from './contexts/Beacon'
+import { Main } from './routes/Main'
+import { CssBaseline } from '@mui/material'
+import { ContractProvider } from './contexts/Contract'
 
-function App() {
+import React from 'react'
+
+export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssBaseline />
+      <SettingsProvider>
+        <TaquitoProvider>
+          <BeaconProvider>
+            <ContractProvider>
+              <Main />
+            </ContractProvider>
+          </BeaconProvider>
+        </TaquitoProvider>
+      </SettingsProvider>
     </div>
-  );
+  )
 }
-
-export default App;
